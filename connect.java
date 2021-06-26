0caed72731b5eb4887259aa48f226203e99cc40c
kyleyiyang <kyleyiyang@gmail.com>
2021-06-25 22:05:43 -0400

    Update connect.java

--- connect.java
+++ connect.java
@@ -44,3 +44,57 @@ class Solution {
         return root;
     }
 }
+
+*********************************
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Node nextLevHead=null,nextLevLeading=null,curr=root;
+        while (curr!=null) {
+            while (curr!=null) {
+                if (curr.left!=null) {
+                    if (nextLevLeading!=null) {
+                        nextLevLeading.next=curr.left;
+                    } else nextLevHead=curr.left;
+                    nextLevLeading=curr.left;
+                }
+                if (curr.right!=null) {
+                    if (nextLevLeading!=null) {
+                        nextLevLeading.next=curr.right;
+                    } else nextLevHead=curr.right;
+                    nextLevLeading=curr.right;
+                }
+                // move to next node
+                curr=curr.next;
+            }
+            //move to next level
+            curr=nextLevHead;
+            nextLevHead=null;
+            nextLevLeading=null;
+        }
+        return root;
+    }
+}
